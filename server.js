95f755da6a5efc4522af208f247043d80254ca62
junhee916 <junhee916@naver.com>
2021-04-17 14:25:05 +0900

    get test

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 const express = require("express")
 const app = express()
 
-app.listen(5000, console.log("서버 접속 완료"))
+const PORT = 5000
+
+app.get("/", (req, res) => {
+    res.json({
+        message : "get 요청 테스트"
+    })
+})
+
+app.listen(PORT, console.log("서버 접속 완료"))
