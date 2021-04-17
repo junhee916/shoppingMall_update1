3e737657527131fb134d13df26f9d494fa4d14e4
junhee916 <junhee916@naver.com>
2021-04-17 14:52:45 +0900

    crud started

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require("express")
 const app = express()
 
+const productRouter = require("./router/product")
+const orderRouter = require("./router/order")
+
+app.use("/product", productRouter)
+app.use("/order", orderRouter)
+
 const PORT = 5000
 
 app.get("/", (req, res) => {
